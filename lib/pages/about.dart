184fe4f31a76b45dbf45216558adea73c4f09631
rival fitrah <rivalfitrah9@gmail.com>
2026-01-15 18:18:37 +0700

    belajar

---
+++ lib/pages/about.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class AboutPage extends StatelessWidget {
+  const AboutPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('About Page'),
+      ),
+      body: const Center(
+        child: Text(
+          'This is the About Page of the Contact App.',
+          style: TextStyle(fontSize: 18),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        child: const Icon(Icons.arrow_back),
+      ),
+    );
+  }
+}
